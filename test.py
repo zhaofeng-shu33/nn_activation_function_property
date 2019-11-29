4dea3d21d607308a22e56b6c2f3f07bf93e5fcd3
zhaofeng-shu33 <616545598@qq.com>
2019-11-29 09:42:17 +0800

    add more test code

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import tensorflow as tf
 from optimization import get_spherical_coordinate
 from optimization import generate_uniform_sample
 
@@ -11,6 +12,16 @@ class TestFunc(unittest.TestCase):
         x_t, y_t = generate_uniform_sample()
         self.assertEqual(x_t.shape.as_list(), [3, 2])
         self.assertEqual(y_t.shape.as_list(), [3, 1])
-        
+        sess = tf.Session()
+        with sess.as_default():
+            x = x_t.eval()
+            x_norm = x.T @ x
+            self.assertAlmostEqual(x_norm[0, 0], 1.0)
+            self.assertAlmostEqual(x_norm[1, 1], 1.0)
+            self.assertAlmostEqual(x_norm[0, 1], 0.0)
+            self.assertAlmostEqual(x_norm[1, 0], 0.0)
+            y = y_t.eval()
+            self.assertAlmostEqual(np.linalg.norm(y), 1.0)
+
 if __name__ == '__main__':
     unittest.main()
