86155fee3b3b948077d3efc77ffcbde8efe6b8f5
zhaofeng-shu33 <616545598@qq.com>
2019-12-26 15:34:36 +0800

    fix seq

--- estimation.py
+++ estimation.py
@@ -4,8 +4,8 @@ import numpy as np
 def coefficient(n, k):
     nplus2 = n + 2
     nplus4 = n + 4
-    a_numerator = k * n + 3 * n + k
-    a_denominator = nplus2 ** 2
+    a_numerator = k * n + 3 * n - k
+    a_denominator = n ** 2
     a = a_numerator / a_denominator
     c_numerator = k * n + 3 * k + n + 2
     c_denominator = -1 * nplus2 * nplus4
@@ -13,7 +13,7 @@ def coefficient(n, k):
     b_numerator = 2 * (n + k + 6)
     b_denominator = -1 * n * nplus4
     b = b_numerator / b_denominator
-    return np.min(np.roots([c, b, a]))
+    return np.min(np.roots([a, b, c]))
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
