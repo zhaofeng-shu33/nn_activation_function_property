65e3a43420291f922bd9a19c731475d26e82722c
zhaofeng-shu33 <616545598@qq.com>
2020-07-28 11:01:42 +0800

    add artificial dataset

--- example_quasi_linear_model.py
+++ example_quasi_linear_model.py
@@ -1,6 +1,7 @@
 # comparison of quasi linear model
 # with linear model (Least Square)
 import numpy as np
+import argparse
 
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
@@ -18,9 +19,32 @@ def get_quasi_linear_prediction_error(X, Y):
     reg.fit(X, Y)
     Y_pred_quasilinear = reg.predict(X)
     return mean_squared_error(Y, Y_pred_quasilinear)
-if __name__ == "__main__":
-    X = [[1,2],[3,4],[5,6],[7,8]]
+
+def artificial_dataset(sample_size=10):
+    # generate data by formula y = z + 0.05 * z^3
+    # z = 3 * x_1 + 4 * x_2 - 2
+    # where (x_1, x_2) is generated from uniform random number
+    X = np.random.random([sample_size, 2])
+    Z = X @ np.array([3, 4]) - 2
+    Y = Z + 0.05 * np.power(Z, 3.0)
+    return (X, Y)
+
+def toy_dataset():
+    X = [[1, 2], [3, 4], [5, 6], [7, 8]]
     Y = [3, 6, 8, 12]
+    return (X, Y)
+
+def compare_two_method(X, Y):
     error_linear = get_linear_prediction_error(X, Y)
     error_quasi_linear = get_quasi_linear_prediction_error(X, Y)
     print(error_linear, error_quasi_linear)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', choices=['toy', 'artificial'], default='toy')
+    args = parser.parse_args()
+    if args.dataset == 'toy':
+        X, Y = toy_dataset()
+    else:
+        X, Y = artificial_dataset()
+    compare_two_method(X, Y)
